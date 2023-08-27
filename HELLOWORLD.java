b98adaf12101a0e5e5d5f5d8dde7a8c30e74f2e9
piyushkankate <110611701+piyushkankate@users.noreply.github.com>
2023-08-27 20:49:10 +0530

    Add files via upload

---
+++ HELLOWORLD.java
@@ -0,0 +1,13 @@
+public class HELLOWORLD {
+
+   /* This is my first java program.
+    * This will print 'Hello World' as the output
+    */
+
+   public static void main(String []args) {
+      System.out.println("Hello World"); // prints Hello World
+int a = 10 ,  b = 20, c;
+c = a + b;
+System.out.println("Addition of two numbers is ="+c); // prints Hello World
+   }
+}
